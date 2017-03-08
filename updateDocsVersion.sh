2ef5f5bf37ecf544ae762a64805fec813c582656
GaneshSPatil <ganeshpl@thoughtworks.com>
2017-03-08 12:14:22 +0530

    Add rest of the docs site

--- updateDocsVersion.sh
+++ updateDocsVersion.sh
@@ -1,40 +1,49 @@
 #!/bin/bash
 
-CURRENT_VERSION="16.4.0"
-VERSION_TO_RELEASE="16.5.0"
-NEXT_VERSION="16.6.0"
+CURRENT_VERSION="17.2.0"
+VERSION_TO_RELEASE="17.3.0"
+NEXT_VERSION="17.4.0"
 REMOTE_NAME="origin"
-
-DOCS_GOCD_REPO_LINK="https://github.com/GaneshSPatil/docs.go.cd"
-DEVELOPER_GOCD_REPO_LINK="https://github.com/GaneshSPatil/developer.go.cd"
 
 declare -a ALL_DOCS_REPO_URLS
 
 ALL_DOCS_REPO_URLS=(
-  "https://github.com/GaneshSPatil/docs.go.cd"
-  "https://github.com/GaneshSPatil/developer.go.cd"
+  "https://github.com/gocd/docs.go.cd"
+  "https://github.com/gocd/developer.go.cd"
+  "https://github.com/gocd/api.go.cd"
+  "https://github.com/gocd/plugin-api.go.cd"
 )
 
 REPOS=(
   "docs.go.cd"
   "developer.go.cd"
+  "api.go.cd"
+  "plugin-api.go.cd"
 )
 
-echo "** Update various doc sites's '/current' and create a release branch"
+echo "** Update various doc sites's '/current' and create a release branch\n"
 
 for i in "${!ALL_DOCS_REPO_URLS[@]}"; do
+
   REPO_URL=${ALL_DOCS_REPO_URLS[$i]}
   REPO=${REPOS[$i]}
   mkdir $REPO
+  echo "Updating docs for: '$REPO'"
+
   echo "Cloning '$REPO_URL' into '$REPO'"
-  # git clone $DOCS_GOCD_REPO_LINK
+  git clone $DOCS_GOCD_REPO_LINK $REPO
   echo "Done: Cloning '$REPO_URL'"
 
   cd $REPO
+  echo "executing in directory: $(pwd)"
   echo "Bump up version to $NEXT_VERSION"
-  # CURRENT_VERSION=$CURRENT_VERSION VERSION_TO_RELEASE=$VERSION_TO_RELEASE  NEXT_VERSION=$NEXT_VERSION REMOTE_NAME=$REMOTE_NAME rake bump_version
+  CURRENT_VERSION=$CURRENT_VERSION VERSION_TO_RELEASE=$VERSION_TO_RELEASE  NEXT_VERSION=$NEXT_VERSION REMOTE_NAME=$REMOTE_NAME rake bump_version
 
   echo "Done: Bump up version to $NEXT_VERSION"
 
   cd ..
+  echo "Removing $REPO/"
+  rm -rf $REPO
+
+  echo "\n"
 done
